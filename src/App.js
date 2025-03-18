8f9fe423d87fc074e0f27a862724734a0d953ea9
rajgahlot1 <rajcommando1.38@gmail.com>
2025-03-18 15:48:40 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import "bootstrap/dist/css/bootstrap.css";
+ import "bootstrap/dist/js/bootstrap.bundle.js";
+import Loginpage from "./Components/Loginpage";
+import "./App.css"
+import { ToastContainer } from "react-toastify";
+import { Route, Routes } from 'react-router-dom';
+import Registerpage from "./Components/Registerpage";
+import Homepage from "./Components/Homepage";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+       <ToastContainer position="top-right" autoClose={2000} />
+       
+<Routes>
+<Route  path="/" element={ <Homepage/>} />
+<Route  path="/Register" element={ <Registerpage/>} />
+<Route  path="/Login" element={ <Loginpage/>} />
 
-export default App;
+</Routes>
+
+    </>
+  );
+};
+
+export default App; 
+// import React from "react";
+// import ChatPage from "./pages/ChatPage";
+
+// function App() {
+//   return (
+//     <div className="app-container">
+//       <ChatPage />
+//     </div>
+//   );
+// }
+
+// export default App;
